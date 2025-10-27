bbee91a730bb0a16eed2ba24d87cf6012bda0898
Padma Santi Ganji <284583@cognizant.com>
2025-10-26 18:21:56 -0700

    Initial commit with Playwright tests

---
+++ tests/Logintest.spec.ts
@@ -0,0 +1,14 @@
+import {test, expect} from '@playwright/test';
+test ('Login Test', async ({page}) => {
+    await page.goto ('https://practicetestautomation.com/practice-test-login/');
+
+    // Fill in username and password
+    await page.locator('#Username').fill('student');
+    await page.locator('#Password').fill('password123');
+
+    // Click the login button
+    await page.getByRole('button', {name: 'Submit'}).click();
+
+    // Verify successful login by checking for a logout button
+    await expect(page.getByRole('button', {name: 'Logout'})).toBeVisible();
+});
